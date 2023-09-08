771cf3c824d8bc8da8ad1f67202cb8e26e915fbe
Ruslan Symonenko <symonenkors@gmail.com>
2023-09-09 01:31:35 +0300

    made basic functional for saving messages to notion

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
 import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+import dotenv from 'dotenv';
+
+import { createNote } from './services/notion.service';
+
+dotenv.config();
+
+const TELEGRAM_TOKEN: string = process.env.TELEGRAM_TOKEN
+  ? process.env.TELEGRAM_TOKEN.toString()
+  : '';
+
+const bot = new Telegraf(TELEGRAM_TOKEN, {
+  handlerTimeout: Infinity,
+});
+
+bot.start((ctx) => {
+  ctx.reply('Welcome to the Notion Archiver Bot');
+});
+
+bot.on(message('text'), async (ctx) => {
+  try {
+    const text: string = ctx.message.text;
+
+    if (!text.trim()) {
+      ctx.reply('Please write some text');
+    }
+
+    const notionResponse = await createNote(text, text);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+bot.launch();
---
+++ src/services/notion.service.ts
@@ -0,0 +1,66 @@
+import { Client } from '@notionhq/client';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const NOTION_SECRET: string = process.env.NOTION_SECRET ? process.env.NOTION_SECRET.toString() : '';
+const NOTION_DB_ID: string = process.env.NOTION_DB_ID ? process.env.NOTION_DB_ID.toString() : '';
+
+const notion = new Client({
+  auth: NOTION_SECRET,
+});
+
+export const createNote = async (title: string, text: string) => {
+  const response = await notion.pages.create({
+    parent: { database_id: NOTION_DB_ID },
+    properties: {
+      Title: {
+        title: [
+          {
+            type: 'text',
+            text: {
+              content: title,
+            },
+          },
+        ],
+      },
+      Text: {
+        rich_text: [
+          {
+            type: 'text',
+            text: {
+              content: text,
+            },
+          },
+        ],
+      },
+      Date: {
+        date: {
+          start: new Date().toISOString(),
+        },
+      },
+    },
+  });
+
+  await notion.blocks.children.append({
+    block_id: response.id,
+    children: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        paragraph: {
+          rich_text: [
+            {
+              type: 'text',
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    ],
+  });
+
+  return response;
+};
